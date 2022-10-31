e5df5db09df5ac985820964c284cfdae46b00225
Nishan <nishanbende@gmail.com>
2022-10-31 16:54:12 +0530

    init

--- apps/next/src/pages/_document.tsx
+++ apps/next/src/pages/_document.tsx
@@ -25,6 +25,10 @@ class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
           />
+          <script
+            src="https://js-cdn.music.apple.com/musickit/v3/musickit.js"
+            async
+          ></script>
         </Head>
         <body className="!max-w-screen">
           <Main />
--- packages/app/screens/home.tsx
+++ packages/app/screens/home.tsx
@@ -1,23 +1,58 @@
-import { ErrorBoundary } from "app/components/error-boundary";
-import { Feed } from "app/components/feed";
-import { Feed as FeedDesktop } from "app/components/feed/feed.md";
+import { useEffect } from "react";
+
 import { withColorScheme } from "app/components/memo-with-theme";
 import { useTrackPageViewed } from "app/lib/analytics";
 
-import { Hidden } from "design-system/hidden";
+let music;
+const initMusicKit = async () => {
+  try {
+    await window.MusicKit.configure({
+      developerToken:
+        "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ilo2VkQ0NTJOMjcifQ.eyJpYXQiOjE2NjY5ODIzODUsImV4cCI6MTY4MjUzNDM4NSwiaXNzIjoiODhUS0hCMjY4VyJ9.fVefAK7d250DZw1CzmQgSPrUy_3X6yz8am0SgWYow9fOdz_H5akyyP_NH2maEOH2V8TfOxMpYvyXVzJzvE7dPw",
+      app: {
+        name: "Showtime",
+        build: "1978.4.1",
+      },
+    });
+  } catch (err) {
+    console.error("Errorr ", err);
+    // Handle configuration error
+  }
+
+  // MusicKit instance is available
+  music = window.MusicKit.getInstance();
+  // const a = await music.authorize();
+  // console.log("RFfrk ", a);
+  console.log("efokfe ", music);
+
+  return music;
+};
 
 const HomeScreen = withColorScheme(() => {
   useTrackPageViewed({ name: "Home" });
 
+  useEffect(() => {
+    if (window.MusicKit) {
+      initMusicKit();
+    } else {
+      document.addEventListener("musickitloaded", async function () {
+        // MusicKit global is now defined.
+        console.log("efkifeijfe ", 123);
+      });
+    }
+  }, []);
+
   return (
-    <ErrorBoundary>
-      <Hidden from="md">
-        <Feed />
-      </Hidden>
-      <Hidden until="md">
-        <FeedDesktop />
-      </Hidden>
-    </ErrorBoundary>
+    <button
+      style={{ marginTop: 100 }}
+      onClick={async () => {
+        const v = await music.authorize();
+        console.log("v ", v, music);
+      }}
+    >
+      {" "}
+      Authorize
+    </button>
   );
 });
 
